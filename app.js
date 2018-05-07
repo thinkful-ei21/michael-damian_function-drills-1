4e43415ae65d8fab9344ba653cfd82d8aaaf5c3f
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-07 13:21:22 -0700

    moved yearOfBirth to own function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 function whoAmI(name, age) {
-    var yearOfBirth = 2018 - age; 
     console.log(`Hi, my name is ${name} and I'm ${age} `);
+    var yob = yearOfBirth(age);
+}
+
+function yearOfBirth(age) {
+    var yearOfBirth = 2018 - age; 
     console.log(`I was born in ${yearOfBirth} `)  
 }
 whoAmI('Mike', 39); 
