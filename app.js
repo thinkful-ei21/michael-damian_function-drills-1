4d065413f2525c864ac7fd072824859a0fd1a27c
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-07 14:16:26 -0700

    added checks for name and age

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function whoAmI(name, age) { 
+
     try {
         var yearOfBirth = 2018 - age;
     }
@@ -8,9 +9,20 @@ function whoAmI(name, age) {
     if (age <= 0) {
         throw "Age can not be negative";
     }
+    if(name === undefined && age === undefined) {
+        throw "Arguments not valid";
+    }
+    else if (typeof name != 'string') {
+        throw "Name must be a string";
+    } 
+    else if (!isNaN(age)) {
+        throw " Age must be a valid number";
+    }
     console.log( `Hi, my name is ${name} and I'm ${age}` ); 
-    var yob = yearOfBirth(age); }
+    var yob = yearOfBirth(age); 
+}
 
 function yearOfBirth(age) { 
     console.log( `I was born in ${yearOfBirth}` )
-} whoAmI('Mike', -5);
+} 
+whoAmI(12, 'mike');
