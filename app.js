2be35a0b86e100295c75eea59a37bfa9518b783c
Damian Marius <damian.mm@live.com>
2018-05-07 13:56:02 -0700

    added error for negative ages

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-function whoAmI(name, age) {
-    console.log(`Hi, my name is ${name} and I'm ${age} `);
-    var yob = yearOfBirth(age);
-}
+function whoAmI(name, age) { 
+    try {
+        var yearOfBirth = 2018 - age;
+    }
+    catch(e) {
+        console.error(e);
+    }
+    if (age <= 0) {
+        throw "Age can not be negative";
+    }
+    console.log( `Hi, my name is ${name} and I'm ${age}` ); 
+    var yob = yearOfBirth(age); }
 
-function yearOfBirth(age) {
-    var yearOfBirth = 2018 - age; 
-    console.log(`I was born in ${yearOfBirth} `)  
-}
-whoAmI('Mike', 39); 
+function yearOfBirth(age) { 
+    console.log( `I was born in ${yearOfBirth}` )
+} whoAmI('Mike', -5);
