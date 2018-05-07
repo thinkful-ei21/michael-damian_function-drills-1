afcd2c7fab462562263e7fb63d0e6c8b736526f2
Damian Marius <damian.mm@live.com>
2018-05-07 13:13:15 -0700

    added var yearOfBirth

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age) {
-    console.log(`Hi, my name is ${name} and I'm ${age} `);  
+    var yearOfBirth = 2018 - age; 
+    console.log(`Hi, my name is ${name} and I'm ${age} `);
+    console.log(`I was born in ${yearOfBirth} `)  
 }
 whoAmI('Mike', 39); 
