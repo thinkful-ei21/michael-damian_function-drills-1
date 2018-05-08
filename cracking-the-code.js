3883a9440ed98c51e4d48d101e7b42de8a848e13
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-07 18:42:42 -0700

    cracking the code-DRAFT

---
+++ cracking-the-code.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function decode(sentence) {
+    let words = sentence.split(' ');
+    console.log(`Words: ${words}`);
+    // 1. define empty string
+    // in for loop 
+    for (let i=0; i<words.length; i++) {
+        let word = words[i];
+        console.log(`Word: ${word}`);
+        let firstLetter = word.charAt(0);
+        console.log(`First char ${firstLetter}`);
+
+   let charcterNumber = 0;
+   let decodedWord = '';
+    switch(firstLetter) {
+        case 'a' :
+            charcterNumber = word.charAt(1);
+            break;
+        case 'b' :
+            charcterNumber = word.charAt(2);
+            break;
+        case 'c' :
+            charcterNumber = word.charAt(3);
+            break;
+        case 'd' :
+            charcterNumber = word.charAt(4);
+            break;
+        default : 
+            charcterNumber = ' ';
+            break;
+    }
+
+    console.log(charcterNumber);
+  //return charcterNumber;
+}
+
+
+}
+
+decode('craft block argon meter bells brown croon droop');
